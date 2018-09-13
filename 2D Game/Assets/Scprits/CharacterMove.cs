963409f5155863da5d7b3ed213b843966a653e75
Andrea Puffer <andrea.p777@stu.nebo.edu>
2018-09-13 10:29:46 -0600

    added left, right and changed jump key. also added platforms

--- 2D Game/Assets/Scprits/CharacterMove.cs
+++ 2D Game/Assets/Scprits/CharacterMove.cs
@@ -20,7 +20,6 @@ public class CharacterMove : MonoBehaviour {
 		
 	}
 	
-	
 	void FixedUpdate () {
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
 	}
@@ -29,7 +28,7 @@ public class CharacterMove : MonoBehaviour {
 	void Update () {
 
 		// This code makes the character jump
-		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
+		if(Input.GetKeyDown (KeyCode.W)&& grounded){
 			Jump();
 		}
 
@@ -40,11 +39,13 @@ public class CharacterMove : MonoBehaviour {
 		}		
 		if(Input.GetKey (KeyCode.A)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+
 	}
 
 	public void Jump(){
 		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
-
 
 }
