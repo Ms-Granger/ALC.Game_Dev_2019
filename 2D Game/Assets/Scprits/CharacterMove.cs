adb316fea2ce6291a126daa6c663a136f205f69e
Andrea Puffer <andrea.p777@stu.nebo.edu>
2018-09-19 10:31:18 -0600

    double jump

--- 2D Game/Assets/Scprits/CharacterMove.cs
+++ 2D Game/Assets/Scprits/CharacterMove.cs
@@ -7,12 +7,16 @@ public class CharacterMove : MonoBehaviour {
 	// Player movement varlables
 	public int moveSpeed;
 	public float JumpHeight;
+	private bool doubleJump;
 	
 	// Player grounded varlables
 	public Transform groundCheck;
 	public float groundCheckRadius;
 	public LayerMask whatIsGround;
 	public bool grounded;
+
+	// Non-Slide Player
+	private float moveVelocity;
 
 
 	// Use this for initialization
@@ -32,20 +36,34 @@ public class CharacterMove : MonoBehaviour {
 			Jump();
 		}
 
+		// Double Jump code
+		if(grounded)
+			doubleJump = false;
+
+		if(Input.GetKeyDown (KeyCode.W)&& !doubleJump && !grounded){
+			Jump();
+			doubleJump = true;
+		}	
+		// Non-Slide Player
+		moveVelocity = 0f;
+
 		// This code makes the character move from side to side using the A&D keys
 		if(Input.GetKey (KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
+			// GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = moveSpeed;
 		}		
 		if(Input.GetKey (KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			moveVelocity = -moveSpeed;
 		}
+
+		GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
+
 
 	}
 
 	public void Jump(){
 		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
-
 }
