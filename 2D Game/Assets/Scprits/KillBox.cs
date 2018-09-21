24f41c52bc8130cc9fbf89ef2c047ba842ee6366
Andrea Puffer <andrea.p777@stu.nebo.edu>
2018-09-21 10:30:46 -0600

    Score

---
+++ 2D Game/Assets/Scprits/KillBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillBox : MonoBehaviour {
+
+
+	void OnTriggerEnter2D (Collider2D other){
+
+		if(other.name == "Hero PC")
+		{
+			Debug.Log("Player Enters Kill Zone");
+			Destroy(other);
+		}
+	}
+	
+}
---
+++ 2D Game/Assets/Scprits/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ScoreText = GetComponent<Text>();
+
+		Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Score < 0)
+			Score = 0;
+
+		ScoreText.text = " " + Score;
+	}
+}
